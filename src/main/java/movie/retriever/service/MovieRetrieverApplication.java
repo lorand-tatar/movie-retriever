78903dff2838b88cc98fa999465c32d47584a316
Lorand Tatar <v-lotatar@hotels.com>
2020-09-11 13:11:07 +0200

    Simply springbootified version

--- src/main/java/movie/retriever/service/MovieRetrieverApplication.java
+++ src/main/java/movie/retriever/service/MovieRetrieverApplication.java
@@ -1,7 +1,20 @@
 package movie.retriever.service;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class MovieRetrieverApplication {
     public static void main(String[] args) {
-        System.out.println("Older movies than 2000: " + new MovieRetrieverService().retrieveClassics(2000));
+        SpringApplication.run(MovieRetrieverApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner print() {
+        return (args) -> {
+            System.out.println("Older movies than 2000: " + new MovieRetrieverService().retrieveClassics(Integer.valueOf(args[0])));
+        };
     }
 }
---
+++ src/test/java/MovieRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package movie.retriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRetrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
